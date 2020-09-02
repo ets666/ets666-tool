044f3422219a006ed615747587b2603c0d522aa2
rsl140 <rsl140@qq.com>
2020-09-02 18:00:34 +0800

    fix: data数据对应

--- src/renderer/views/index/components/setting.vue
+++ src/renderer/views/index/components/setting.vue
@@ -12,17 +12,17 @@
         <div class="mb10">
           选择存档:
         </div>
-        <el-select v-model="value" size="mini" placeholder="请选择档案" class="mb10">
+        <el-select v-model="profile" size="mini" placeholder="请选择档案" class="mb10">
           <el-option
-            v-for="item in options"
+            v-for="item in profileOptions"
             :key="item.value"
             :label="item.label"
             :value="item.value">
           </el-option>
         </el-select>
-        <el-select v-model="value" size="mini" placeholder="请选择存档" class="mb10">
+        <el-select v-model="save" size="mini" placeholder="请选择存档" class="mb10">
           <el-option
-            v-for="item in options"
+            v-for="item in saveOptions"
             :key="item.value"
             :label="item.label"
             :value="item.value">
@@ -34,10 +34,10 @@
         <div class="mb10">
           货物同步:
         </div>
-        <el-checkbox v-model="checked" class="mb10">
+        <el-checkbox v-model="job.synTody" class="mb10">
           同步今日联运任务
         </el-checkbox>
-        <el-checkbox v-model="checked">
+        <el-checkbox v-model="job.moveToCargo">
           移动车辆至起点货场
         </el-checkbox>
       </div>
@@ -48,25 +48,25 @@
         <div class="mb10">
           存档修改:
         </div>
-        <el-checkbox v-model="checked" class="mb10">
+        <el-checkbox v-model="setting.money" class="mb10">
           修改金钱为1亿
         </el-checkbox>
-        <el-checkbox v-model="checked" class="mb10">
+        <el-checkbox v-model="setting.level" class="mb10">
           修改玩家级别为100级
         </el-checkbox>
-        <el-checkbox v-model="checked" class="mb10">
+        <el-checkbox v-model="setting.skills" class="mb10">
           解锁全部技能
         </el-checkbox>
-        <el-checkbox v-model="checked" class="mb10">
+        <el-checkbox v-model="setting.city" class="mb10">
           解锁全部城市
         </el-checkbox>
-        <el-checkbox v-model="checked" class="mb10">
+        <el-checkbox v-model="setting.garage" class="mb10">
           解锁全部车库
         </el-checkbox>
-        <el-checkbox v-model="checked" class="mb10">
+        <el-checkbox v-model="setting.damage" class="mb10">
           修复全部车损
         </el-checkbox>
-        <el-checkbox v-model="checked" class="mb10">
+        <el-checkbox v-model="setting.oil" class="mb10">
           全部车辆满油
         </el-checkbox>
       </div>
@@ -77,31 +77,31 @@
         </div>
         <div class="text_box mb10">
           <span class="text">服务器:</span>
-          <el-input placeholder="服务器" size="mini" disabled v-model="input1" />
+          <el-input placeholder="服务器" size="mini" disabled v-model="jobInfo.server" />
         </div>
         <div class="text_box mb10">
           <span class="text">起点城市:</span>
-          <el-input placeholder="服务器" size="mini" disabled v-model="input1" />
+          <el-input placeholder="服务器" size="mini" disabled v-model="jobInfo.startCity" />
         </div>
         <div class="text_box mb10">
           <span class="text">起点货场:</span>
-          <el-input placeholder="服务器" size="mini" disabled v-model="input1" />
+          <el-input placeholder="服务器" size="mini" disabled v-model="jobInfo.startCargo" />
         </div>
         <div class="text_box mb10">
           <span class="text">终点城市:</span>
-          <el-input placeholder="服务器" size="mini" disabled v-model="input1" />
+          <el-input placeholder="服务器" size="mini" disabled v-model="jobInfo.endCity" />
         </div>
         <div class="text_box mb10">
           <span class="text">终点货场:</span>
-          <el-input placeholder="服务器" size="mini" disabled v-model="input1" />
+          <el-input placeholder="服务器" size="mini" disabled v-model="jobInfo.endCargo" />
         </div>
         <div class="text_box mb10">
           <span class="text">货物:</span>
-          <el-input placeholder="服务器" size="mini" disabled v-model="input1" />
+          <el-input placeholder="服务器" size="mini" disabled v-model="jobInfo.cargo" />
         </div>
         <div class="text_box mb10">
           <span class="text">预估里程:</span>
-          <el-input placeholder="服务器" size="mini" disabled v-model="input1" />
+          <el-input placeholder="服务器" size="mini" disabled v-model="jobInfo.mileage" />
         </div>
 
 
@@ -135,26 +135,39 @@ export default {
   data () {
     return {
       savePath: '',
-      checked: true,
-      value: '',
-      input1: 'S1',
-      options: [{
+      tody: moment().format('yyyy年MM月DD日'),
+      profile: '',
+      save: '',
+      profileOptions: [{
         value: '选项1',
         label: '黄金糕'
-      }, {
-        value: '选项2',
-        label: '双皮奶'
-      }, {
-        value: '选项3',
-        label: '蚵仔煎'
-      }, {
-        value: '选项4',
-        label: '龙须面'
-      }, {
-        value: '选项5',
-        label: '北京烤鸭'
       }],
-      tody: moment().format('yyyy年MM月DD日')
+      saveOptions: [{
+        value: '选项1',
+        label: '黄金糕'
+      }],
+      setting: {
+        money: false,
+        level: false,
+        skills: false,
+        city: false,
+        garage: false,
+        damage: false,
+        oil: false
+      },
+      job: {
+        synTody: false,
+        moveToCargo: false
+      },
+      jobInfo: {
+        server: '',
+        startCity: '',
+        startCargo: '',
+        endCity: '',
+        endCargo: '',
+        cargo: '',
+        mileage: ''
+      }
     }
   },
   created () {
